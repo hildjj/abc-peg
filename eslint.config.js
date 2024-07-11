61177b453d9007ed35f883288771f690b31b52b9
Joe Hildebrand <joe-github@cursive.net>
2024-07-11 16:41:40 -0600

    I don't like turning this rule off, but i can't understand why it's firing.

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ export default [
   {
     ignores: [
       'lib/**',
+      '**/*.d.ts',
     ],
   },
   ...base,
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line n/no-missing-import
 import {parse} from '../lib/abc.js';
 
 export function foo(text: string): unknown {
